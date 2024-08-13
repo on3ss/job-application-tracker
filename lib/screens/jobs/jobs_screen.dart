f9f1b878227a8fdcc84b1aeabfda7132f2c35e6e
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-08-13 11:50:52 +0530

    build

--- lib/screens/jobs/jobs_screen.dart
+++ lib/screens/jobs/jobs_screen.dart
@@ -31,7 +31,7 @@ class JobApplicationListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
       child: ListTile(
         title: Text(application.post),
         subtitle: Column(
@@ -41,12 +41,12 @@ class JobApplicationListItem extends StatelessWidget {
             Text('Applied via: ${application.appliedVia}'),
             // Text('Date: ${application.applicationDate.toLocal().toString().split(' ')[0]}'),
             if (application.link != null)
-              Text('Link: ${application.link}', style: const TextStyle(color: Colors.blue)),
+              Text('Link: ${application.link}',
+                  style: const TextStyle(color: Colors.blue)),
           ],
         ),
         trailing: const Icon(Icons.chevron_right),
-        onTap: () {
-        },
+        onTap: () {},
       ),
     );
   }
