faca5c6bf92fd0c5ce80d4d8808c6de12f3e9dcf
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-09-03 16:04:50 +0530

    added stages list in tabs

--- lib/screens/job_applications/job_application_screen.dart
+++ lib/screens/job_applications/job_application_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:job_tracker/dev_only/dummy_data.dart';
+import 'package:job_tracker/dev_only/models.dart';
 import 'package:job_tracker/screens/job_applications/widgets/icon_with_text.dart';
 import 'package:job_tracker/widgets/custom_appbar.dart';
 
@@ -48,15 +50,68 @@ class JobApplicationScreen extends HookWidget {
               child: TabBarView(
                 controller: tabController,
                 children: const [
-                  Text("All"),
-                  Text("Pending"),
-                  Text("Done"),
+                  ApplicationStageList(),
+                  ApplicationStageList(),
+                  ApplicationStageList(),
                 ],
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ApplicationStageList extends HookWidget {
+  const ApplicationStageList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<JobApplicationStage> stages =
+        useMemoized(() => List.generate(10, (index) => generateStage(1)));
+    return ListView.separated(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height * 0.05,
+      ),
+      itemBuilder: (context, index) => ListTile(
+        title: Text(
+          stages[index].name,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              stages[index].description,
+              style: Theme.of(context).textTheme.bodySmall,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 8.0),
+            Wrap(
+              direction: Axis.horizontal,
+              spacing: 4.0,
+              children: stages[index]
+                  .subjects
+                  .map(
+                    (subject) => Badge(
+                      label: Text(subject),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      textStyle:
+                          Theme.of(context).textTheme.labelMedium!.copyWith(
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                    ),
+                  )
+                  .toList(),
             )
           ],
         ),
       ),
+      separatorBuilder: (_, __) => const Divider(),
+      itemCount: stages.length,
     );
   }
 }
