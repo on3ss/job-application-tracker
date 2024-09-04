b9019b23c00519af7f81b6c8482c20f189df440b
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-09-04 16:46:53 +0530

    widgetised

--- lib/screens/job_applications/job_application_screen.dart
+++ lib/screens/job_applications/job_application_screen.dart
@@ -21,71 +21,110 @@ class JobApplicationScreen extends HookWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              margin: const EdgeInsets.all(8.0),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet diam purus, pulvinar aliquam dolor malesuada sit amet.'),
-                  SizedBox(height: 8.0),
-                  IconWithText(
-                    icon: HugeIcons.strokeRoundedBuilding04,
-                    text: "Meghalaya Public Service Commission",
-                  ),
-                  SizedBox(height: 4.0),
-                  IconWithText(
-                    icon: HugeIcons.strokeRoundedCalendarAdd01,
-                    text: "28 December 2024",
-                  ),
-                ],
-              ),
-            ),
-            TabBar(
-              controller: tabController,
-              tabs: [
-                const Tab(child: Text("All")),
-                Tab(
-                  child: Row(
-                    children: [
-                      Icon(
-                        HugeIcons.strokeRoundedLoading01,
-                        color: Theme.of(context).colorScheme.tertiary,
-                        size: 16.0,
-                      ),
-                      const SizedBox(width: 4.0),
-                      const Text("Pending"),
-                    ],
-                  ),
-                ),
-                Tab(
-                  child: Row(
-                    children: [
-                      Icon(
-                        HugeIcons.strokeRoundedCheckmarkCircle02,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 16.0,
-                      ),
-                      const SizedBox(width: 4.0),
-                      const Text("Done"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: tabController,
-                children: const [
-                  ApplicationStageList(),
-                  ApplicationStageList(),
-                  ApplicationStageList(),
-                ],
-              ),
-            ),
+            const JobApplicationInfo(),
+            ApplicationStageListTabs(tabController: tabController),
+            ApplicationStageListViews(tabController: tabController),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ApplicationStageListViews extends StatelessWidget {
+  const ApplicationStageListViews({
+    super.key,
+    required this.tabController,
+  });
+
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TabBarView(
+        controller: tabController,
+        children: const [
+          ApplicationStageList(),
+          ApplicationStageList(),
+          ApplicationStageList(),
+        ],
+      ),
+    );
+  }
+}
+
+class JobApplicationInfo extends StatelessWidget {
+  const JobApplicationInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet diam purus, pulvinar aliquam dolor malesuada sit amet.'),
+          SizedBox(height: 8.0),
+          IconWithText(
+            icon: HugeIcons.strokeRoundedBuilding04,
+            text: "Meghalaya Public Service Commission",
+          ),
+          SizedBox(height: 4.0),
+          IconWithText(
+            icon: HugeIcons.strokeRoundedCalendarAdd01,
+            text: "28 December 2024",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ApplicationStageListTabs extends StatelessWidget {
+  const ApplicationStageListTabs({
+    super.key,
+    required this.tabController,
+  });
+
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      controller: tabController,
+      tabs: [
+        const Tab(child: Text("All")),
+        Tab(
+          child: Row(
+            children: [
+              Icon(
+                HugeIcons.strokeRoundedLoading01,
+                color: Theme.of(context).colorScheme.tertiary,
+                size: 16.0,
+              ),
+              const SizedBox(width: 4.0),
+              const Text("Pending"),
+            ],
+          ),
+        ),
+        Tab(
+          child: Row(
+            children: [
+              Icon(
+                HugeIcons.strokeRoundedCheckmarkCircle02,
+                color: Theme.of(context).colorScheme.primary,
+                size: 16.0,
+              ),
+              const SizedBox(width: 4.0),
+              const Text("Done"),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -137,53 +176,67 @@ class ApplicationStageList extends HookWidget {
       padding: EdgeInsets.only(
         bottom: MediaQuery.of(context).size.height * 0.05,
       ),
-      itemBuilder: (context, index) => ListTile(
-        title: Text(
-          stages[index].name,
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              stages[index].description,
-              style: Theme.of(context).textTheme.bodySmall,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 8.0),
-            Wrap(
-              direction: Axis.horizontal,
-              spacing: 8.0,
-              children: stages[index]
-                  .subjects
-                  .map(
-                    (subject) => Badge(
-                      label: Text(subject),
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      textStyle:
-                          Theme.of(context).textTheme.labelMedium!.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: Theme.of(context).colorScheme.onPrimary,
-                              ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ],
-        ),
-        trailing: Icon(
-          stages[index].isDone
-              ? HugeIcons.strokeRoundedCheckmarkCircle02
-              : HugeIcons.strokeRoundedLoading01,
-          color: stages[index].isDone
-              ? Theme.of(context).colorScheme.primary
-              : Theme.of(context).colorScheme.tertiary,
-          size: 20.0,
-        ),
-      ),
+      itemBuilder: (context, index) =>
+          ApplicationStageListItem(stage: stages[index]),
       separatorBuilder: (_, __) => const Divider(),
       itemCount: stages.length,
+    );
+  }
+}
+
+class ApplicationStageListItem extends StatelessWidget {
+  const ApplicationStageListItem({
+    super.key,
+    required this.stage,
+  });
+
+  final JobApplicationStage stage;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        stage.name,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            stage.description,
+            style: Theme.of(context).textTheme.bodySmall,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 8.0),
+          Wrap(
+            direction: Axis.horizontal,
+            spacing: 8.0,
+            children: stage.subjects
+                .map(
+                  (subject) => Badge(
+                    label: Text(subject),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    textStyle:
+                        Theme.of(context).textTheme.labelMedium!.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                  ),
+                )
+                .toList(),
+          ),
+        ],
+      ),
+      trailing: Icon(
+        stage.isDone
+            ? HugeIcons.strokeRoundedCheckmarkCircle02
+            : HugeIcons.strokeRoundedLoading01,
+        color: stage.isDone
+            ? Theme.of(context).colorScheme.primary
+            : Theme.of(context).colorScheme.tertiary,
+        size: 20.0,
+      ),
     );
   }
 }
